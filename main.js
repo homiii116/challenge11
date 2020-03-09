0a8c373e1a723b81d743e3c44ccd2c9f11f4d6cc
miho yamagata <miho.yamagata@gmail.com>
2020-03-09 23:41:22 +0900

    edit

--- main.js
+++ main.js
@@ -1,21 +1,25 @@
 // モーダル要素の取得
+const modal = document.getElementById('easyModal');
 
 
 // 開くボタン取得
+const buttonOpen = document.getElementById('modalOpen');
 
 
 // 閉じるボタン取得
-
+const buttonClose = document.getElementsByClassName('modalClose');
 
 
 // クリックで開くイベントリスナー
-
+buttonOpen.addEventListener('click', function() {
+  modal.style.display = 'block';
+})
 
 // クリックで閉じるイベントリスナー
 
 
-// モーダル以外の場所クリックのイベントリスナー
 
+// モーダル以外の場所クリックのイベントリスナー
 
 
 // モーダルオープンの関数
@@ -24,4 +28,4 @@
 // モーダルを閉じる関数
 
 
-// モーダル以外がクリックされた時に閉じる関数
+// モーダル以外がクリックされた時に閉じる関数//
